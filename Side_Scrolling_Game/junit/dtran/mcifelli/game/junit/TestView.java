1d783087ddb0f4590ee26a0ff9cf83674086fac5
Dung Tran <dtran2@ycp.edu>
2013-04-13 21:02:55 -0400

    Started the UI for testing the game objects. Need object values in order to move forward.

---
+++ Side_Scrolling_Game/junit/dtran/mcifelli/game/junit/TestView.java
@@ -0,0 +1,17 @@
+package dtran.mcifelli.game.junit;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+/**
+ * Testing game objects in a graphic
+ * setting
+ * @author dtran2
+ *
+ */
+public class TestView extends JPanel{
+	private static final Color DEFAULT_COLOR = Color.PINK; //Used to make debugging a bit easier
+	private static final Color PLAYER_COLOR = Color.GRAY;
+
+}
