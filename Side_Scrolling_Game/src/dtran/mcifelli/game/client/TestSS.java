4b05d43bb5a44c1fb6b146c94f0f63153e15ad8d
Dung Q. Tran <dtran2@ycp.edu>
2013-05-17 20:18:35 -0400

    Remade runnable gui

---
+++ Side_Scrolling_Game/src/dtran/mcifelli/game/client/TestSS.java
@@ -0,0 +1,44 @@
+package dtran.mcifelli.game.client;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+import dtran.mcifelli.game.objects.Game;
+
+
+
+
+/**
+ * Runnable GUI to test object classes in 
+ * a graphical envrionment 
+ * @author dtran2
+ *
+ */
+public class TestSS {
+	public static void main(String[] args){
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				// Create the Game object that represents the game state
+				Game game = new Game();
+				
+				// Create the KabomView that will visualize the game state
+				TestView view = new TestView(game);
+
+				// Create a frame (top-level window) to enclose the KaboomView
+				JFrame frame = new JFrame();
+				frame.setTitle("Tetris!");
+				frame.setContentPane(view);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.pack();
+				
+				// Start the game!
+				view.startGame();
+				
+				// Make the frame visible
+				frame.setVisible(true);
+			}
+		});
+	
+	}
+}
